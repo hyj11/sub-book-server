4c538ce78cfe802a6a5822cee91368f69d9c0511
huangyujiao <huangyujiao@zjaisino.com>
2018-07-17 09:58:25 +0800

    server服务

---
+++ src/main/java/com/sim/SubBookServerApplication.java
@@ -0,0 +1,12 @@
+package com.sim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubBookServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubBookServerApplication.class, args);
+	}
+}
